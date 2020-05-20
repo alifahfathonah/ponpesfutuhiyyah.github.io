a1d99e3dd453783f53f7a24e6d6624fc73dc41ad
ponpesfutuhiyyah <novanthekid@gmail.com>
2020-05-20 10:30:28 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Penerimaan Siswa Baru</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="assets/css/material-dashboard.css?v=1.2.0" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="assets/css/demo.css" rel="stylesheet" />
+    <!--     Fonts and icons     -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="assets/css/main.css">
+
+
+	<!--Font -->
+	<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+	<!-- Favicon and touch icons -->
+	<link rel="shortcut icon" href="assets/ico/favicon.png">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="tugas-dbd-ardan/assets/ico/apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="tugas-dbd-ardan/assets/ico/apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="tugas-dbd-ardan/assets/ico/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="tugas-dbd-ardan/assets/ico/apple-touch-icon-57-precomposed.png">
+</head>
+	<div class="grid">
+
+		<div class="row">
+			<div align="center">
+				<img  src="assets/img/logo.png" class="img-responsive"  alt="respinsive image" width="150"  >
+			</div>
+			<div class="col-10" align="center">
+				
+				<h2 >Sistem Pendaftaran Santri futuhiyyah</h2>
+				<h4 > Anwarul Huda, Mencetak Generasi Ibadurrahman Berlandaskan Ahlul Sunnah Wal Jamma'ah</h6>
+				<h4>PONDOK PESANTREN FUTUHIYYAH MRANGGEN</h6>
+				<h4>MENCETAK GENERASI Ahlul Sunnah Wal Jamma'ah</h6>
+				<h5>Alamat : Jalan Suluburan Barat No.31, Mranggen, Brumbung, Kecamatan Mranggen, Kabupaten Demak, Jawa Tengah</h8>
+			</div>
+		</div>
+		<!--	<div class="row">
+		<div class="col-12 " align="center">
+		<img src="assets/img/.jpg" class="img-responsive"  alt="responsive image" width="1000" >
+	
+		</div>
+		</div>	-->
+<body>
+		<div class="container">
+	    <div class="row">
+	    	<div class="col-md-10 col-md-offset-2">
+	    		<div class="card-background">
+	    		
+	    			<div class="col-md-6 col-lg-5 col-sm-12 student">
+	    				<center>
+	    					<a href="login.php">
+		    					<img src="assets/img/student.png" alt="Login">
+								<h3>Login</h3>
+							</a>
+	    				</center>
+	    			</div>
+
+	    			<div class="col-md-6 col-lg-5 col-sm-12 note">
+	    				<center>
+	    					<a href="daftar_akun.php">
+		    					<img src="assets/img/note.png" alt="Pendaftaran Siswa Baru">
+								<h3>Daftar</h3>
+							</a>
+	    				</center>
+	    			</div>	
+				
+	    		</div>
+	    	</div>
+	    </div>
+	</div>
+	
+
+
+
+</body>
+</html>
